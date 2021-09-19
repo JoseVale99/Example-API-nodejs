e4d380a6af6839d1f585fa0a47987e0136dc52b8
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-19 00:21:02 -0500

    install dependencies

---
+++ dbCategory.js
@@ -0,0 +1,14 @@
+var config = require('./bdconfig'); // instance of config object
+
+
+const sql = require('mysql'); // package mysql connection
+
+
+
+async function getCategory(){
+    try {
+
+    }catch(err){
+        console.log(err);
+    }
+}
