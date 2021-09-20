b41b9502b68b944a02d310c111627adfe6fe1fbe
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-20 01:28:19 -0500

    API asyn func new data

--- model/dbCategory.js
+++ model/dbCategory.js
@@ -1,41 +1,58 @@
 const conexion = require("../bdconfig")
 
-async function getCategory(){
+async function getCategory() {
     try {
- return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
             conexion.query('SELECT * FROM categories',
                 (err, resultados) => {
                     if (err) reject(err);
                     else resolve(resultados);
-                    
+
                 });
         });
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
 
-async function getCategoryID(id){
+async function getCategoryID(id) {
     try {
         return new Promise((resolve, reject) => {
             conexion.query(`SELECT * FROM categories WHERE id = ?`,
                 [id],
                 (err, resultados) => {
                     if (err) reject(err);
-                    else 
-                    resolve(resultados);
+                    else
+                        resolve(resultados);
                     console.log(resultados);
                 });
         });
-       
-           }catch(error){
-               console.log(error);
-           }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function createCategory(cat_name, cat_obs) {
+    try {
+        return new Promise((resolve, reject) => {
+            conexion.query(`INSERT INTO categories
+        (cat_name, cat_obs)
+        values
+        (?, ?)`,
+                [cat_name, cat_obs], (err, resultados) => {
+                    if (err) reject(err);
+                    else resolve(resultados.insertId);
+                });
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
-module.exports ={
-    getCategory : getCategory,
-    getCategoryID : getCategoryID   
+module.exports = {
+    getCategory: getCategory,
+    getCategoryID: getCategoryID,
+    createCategory: createCategory
 };
